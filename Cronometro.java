972010d433dbaf7ec635cae5ec76b07115706c6c
Alejandro Galvis Florez <90481045+Agalvf@users.noreply.github.com>
2022-11-12 23:37:50 -0500

    Agregando metodos y botones

--- Cronometro.java
+++ Cronometro.java
@@ -4,6 +4,8 @@
  */
 package Pruebas;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,32 +15,63 @@ import javax.swing.JLabel;
  * @author ASUS
  */
 public class Cronometro {
+
+    private static int corredor = 0;
+    
     public static void main(String[] args) {
+        int filas = 10000;
         Horas hora = new Horas();
         Minutos minuto = new Minutos(hora);
         Segundos segundo = new Segundos(minuto);
-
-        hora.start();
-        minuto.start();
-        segundo.start();
         
-        JFrame ventana = new JFrame("Reloj");
-        ventana.setSize(300, 300);
+        JFrame ventana = new JFrame("Cronometro");
+        ventana.setSize(400, 400);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setLayout(null);
         ventana.setVisible(true);
 
-        JLabel labelHora = new JLabel();
-        labelHora.setBounds(10, 10, 100, 30);
-        labelHora.setText(hora.getContador()+ ":" + minuto.getContador()+ ":" + segundo.getContador());
-        ventana.add(labelHora);
+        JLabel label = new JLabel("00:00:00");
+        label.setBounds(150, 150, 100, 100);
+        ventana.add(label);
 
-        JButton boton = new JButton("Boton");
-        boton.setBounds(10, 50, 100, 30);
+        JButton boton = new JButton("Iniciar");
+        boton.setBounds(150, 250, 100, 50);
         ventana.add(boton);
-              
-        while(true){
-            labelHora.setText(hora.getContador()+ ":" + minuto.getContador()+ ":" + segundo.getContador());
+
+        JButton agregar = new JButton("Agregar");
+        agregar.setBounds(150, 300, 100, 50);
+        ventana.add(agregar);
+
+        int tiempoCorredores [][] = new int[filas][3];
+        
+        boton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hora.start();
+                minuto.start();
+                segundo.start();
+            }
+        });
+
+        agregar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tiempoCorredores[corredor][0] = hora.getContador();
+                tiempoCorredores[corredor][1] = minuto.getContador();
+                tiempoCorredores[corredor][2] = segundo.getContador();
+                corredor++;
+                for(int i = 0; i < corredor; i++){
+                    for (int j = 0; j < 3; j++) {
+                        System.out.println("Corredor: "  + i +  " Tiempo: " + tiempoCorredores[i][j]);
+                    }
+                    System.out.println("\n");
+                }
+            }
+        });
+
+   
+        while (true) {
+            label.setText(hora.getContador() + ":" + minuto.getContador() + ":" + segundo.getContador());
         }
     }
 }
